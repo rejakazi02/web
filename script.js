bb6bd191ea797351b5709782e87c5d0625ba5156
Reja Kazi <70040672+rejakazi02@users.noreply.github.com>
2021-02-21 01:40:07 +0600

    Update script.js

--- script.js
+++ script.js
@@ -35,7 +35,7 @@ $(document).ready(function(){
 
     // typing text animation script
     var typed = new Typed(".typing", {
-        strings: ["Web Developer", "Freelancer"],
+        strings: ["Web-Developer", "Freelancer"],
         typeSpeed: 100,
         backSpeed: 60,
         loop: true
